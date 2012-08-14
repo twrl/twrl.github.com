c3150d55a3d063fd995b2cbf3b58a3f2da04df0a
Tom Robbins <tomrobbins@gmx.com>
2012-08-14 20:26:43 +0100

    Latest build.

--- js/site/advnav.js
+++ js/site/advnav.js
@@ -1,9 +1,10 @@
 // This module defines the behaviours of the Expanding Navigation Panel and it's controls
-define(['jquery'], function ($) {
+define(['jquery', 'domReady!'], function ($) {
 
-	$($('#l-advnavctl-ctrl').click(function () {
+	$('#l-advnavctl-ctrl').click(function (e) {
+	    e.stopImmediatePropagation();
 		$('#l-advnav').slideToggle();
 		$('#advnavctl-show, #advnavctl-hide').toggle();
-	}));
+	});
     
 });
