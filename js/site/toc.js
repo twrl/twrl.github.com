e50312a741b3fcce52ff64d1dab5b42a54e74190
Tom Robbins <00.twrl@gmail.com>
2012-09-26 18:03:52 +0100

    Latest build of site

--- js/site/toc.js
+++ js/site/toc.js
@@ -4,16 +4,17 @@ define(['jquery', 'domReady!'], function ($) {
 	$('#markdown-toc').detach().prependTo('#l-article-toc');
 
 	$('#l-article-toc li a').click(function() {
-		$('html,body').animate({scrollTop: $($(this).attr('href')).offset().top});
+		$($(this).attr('href')).scrollTo();
 	});
 
 	var up = $('<a href="#">To Top</a>');
+	up.append('<img src="/images/icons/19.png" class="icon"/>');
 	up.click(function () {
 		$('html,body').animate({scrollTop: 0});
 	});
 	var l = $("<span></span>");
-	l.css('font-size', '10pt').css('font-weight', 'normal').css('float', 'right').append("[", up, "]");
+	l.css('font-size', '10pt').css('font-weight', 'normal').css('float', 'right').append(up);
 
-	$('h1, h2, h3, h4, h5', $('article > div')).append(l);
+	$('h1, h2, h3, h4, h5', $('article .content')).append(l);
 
 });
